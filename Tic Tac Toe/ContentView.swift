6ae43ba5ceb83af926078af4499fc3656689bac5
Ethan Davis <edavis@bsd220.org>
2023-11-15 09:45:43 -0600

    completed check winner function

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -52,8 +52,18 @@ struct ContentView: View {
         .padding()
     }
     private func checkForWinner() {
-        if moves[0] != "" && moves[0] == moves[1] && moves[1] == moves[2] {
-            winMessage = "\(moves[0]) is the winner!"
+        checkLine(a: 0, b: 1, c: 2) // row
+        checkLine(a: 3, b: 4, c: 5)
+        checkLine(a: 6, b: 7, c: 8)
+        checkLine(a: 0, b: 3, c: 6) // column
+        checkLine(a: 1, b: 4, c: 7)
+        checkLine(a: 2, b: 5, c: 8)
+        checkLine(a: 0, b: 4, c: 8) // diagonal
+        checkLine(a: 2, b: 4, c: 6)
+    }
+    private func checkLine(a: Int, b: Int, c: Int) {
+        if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
+            winMessage = "\(moves[a]) is the winner!"
             gameOver = true
         }
     }
